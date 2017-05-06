0f56f2709606df22a7a615986463c59344a7c558
Chris Sperandio <chris@sperand.io>
2017-05-05 17:10:30 -0700

    Update main.go

--- cmd/postgres/main.go
+++ cmd/postgres/main.go
@@ -1,7 +1,7 @@
 package main
 
 import "github.com/segment-sources/sqlsource"
-import "github.com/segment-sources/postgres"
+import "github.com/segment-sources/source-postgres"
 
 func main() {
 	sqlsource.Run(&postgres.Postgres{})
